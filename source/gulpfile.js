c4f6a32323afa31d52e59c6c283ced9c24a093e9
Bob den Otter <bob@twokings.nl>
2017-11-07 08:24:22 +0100

    Initial commit 🚀

---
+++ source/gulpfile.js
@@ -0,0 +1,58 @@
+var gulp = require('gulp');
+var $    = require('gulp-load-plugins')();
+var argv = require('yargs').argv;
+
+// Check for --production flag
+var PRODUCTION = !!(argv.production);
+
+// Define base paths for Sass and Javascript.
+var sassPaths = [
+    'scss/',
+];
+
+var javascriptFiles = [
+    'bower_components/jquery/dist/jquery.js',
+];
+
+// Set up 'sass' task.
+gulp.task('sass', function() {
+
+  return gulp.src('scss/styleguide.scss')
+    .pipe($.sourcemaps.init())
+    .pipe($.sass({
+      includePaths: sassPaths
+    })
+      .on('error', $.sass.logError))
+    .pipe($.autoprefixer({
+      browsers: ['last 2 versions', 'ie >= 9']
+    }))
+    .pipe($.if(PRODUCTION, $.cssnano()))
+    .pipe($.if(!PRODUCTION, $.sourcemaps.write()))
+    .pipe(gulp.dest('../css'));
+});
+
+// Set up 'compress' task.
+gulp.task('compress', function() {
+  return gulp.src('javascript/*.js')
+    .pipe($.uglify())
+    .pipe(gulp.dest('../javascript'));
+});
+
+gulp.task('copyjavascript', function() {
+   gulp.src(javascriptFiles)
+   .pipe(gulp.dest('javascript'));
+});
+
+gulp.task('setproduction', function() {
+  PRODUCTION = true;
+});
+
+// Set up 'default' task, with watches.
+gulp.task('default', ['copyjavascript', 'sass', 'compress'], function() {
+  gulp.watch(['scss/**/*.scss'], ['sass']);
+  gulp.watch(['javascript/**/*.js'], ['compress']);
+});
+
+// Set up 'build' task, without watches and force 'production'.
+gulp.task('build', ['setproduction', 'copyjavascript', 'sass', 'compress']);
+
